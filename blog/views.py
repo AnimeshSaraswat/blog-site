87e777960d83c840f360f5dab2ce2deb9717b38d
Animesh Saraswat <animesh@Animesh-MacBook-Air.local>
2025-04-18 13:21:48 +0530

    initial push

---
+++ blog/views.py
@@ -0,0 +1,91 @@
+from django.shortcuts import render
+from datetime import date
+
+all_posts = [
+    {
+        "slug": "hike-in-the-woods",
+        "image": "elephant.jpg",
+        "author": "Animesh",
+        "date": date(2025, 2, 21),
+        "title": "Hiking in the Woods",
+        "excerpt": "There's nothing quite like the thrill of searching for an elephant in the woods! And I never could've guessed what I stumbled upon while I was lost in the excitement of the hunt!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """,
+    },
+    {
+        "slug": "programming-is-fun",
+        "image": "coding.jpg",
+        "author": "Animesh",
+        "date": date(2024, 3, 10),
+        "title": "Programming Is Great!",
+        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """,
+    },
+    {
+        "slug": "the-taste-of-coffee",
+        "image": "coffee.jpg",
+        "author": "Animesh",
+        "date": date(2023, 8, 5),
+        "title": "Coffee At Its Best",
+        "excerpt": "There's absolutely nothing like the first sip of a perfectly brewed coffee! And I never expected the burst of flavor that hit me while I was simply savoring the moment!",
+        "content": """
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
+          aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
+          velit labore vero culpa ad mollitia? Quis architecto ipsam nemo. Odio.
+        """,
+    },
+]
+
+
+def get_date(post):
+    return post["date"]
+
+
+# Create your views here.
+
+
+def landing_page(request):
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+    return render(request, "blog/landing_page.html", {"posts": latest_posts})
+
+
+def posts(request):
+    return render(request, "blog/all-posts.html", {"all_posts": all_posts})
+    pass
+
+
+def post_details(request, slug):
+    identified_post = next(post for post in all_posts if post["slug"] == slug)
+    return render(request, "blog/post-detail.html", {"post": identified_post})
